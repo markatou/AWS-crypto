e32441c42e0ab1bfc9a820e6c685620b47edd7fa
EC2 Default User <ec2-user@ip-172-31-15-203.us-east-2.compute.internal>
2017-12-02 01:09:49 +0000

    Timing data update

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
-echo "How long does SVP take for different matrices sizes" >> timing.txt
+echo "How long does SVP take for different matrices sizes" >> timing-c2-2xlarge.txt
 # To create matrices ./fplll/fplll/latticegen u $i 10 >> matrices/matrix$i.txt
-for i in {10..150..5}
+for i in {10..150..10}
   do
      #(time echo a) &>> foo 
-     echo "We have $i dimensions" >> timing.txt
-     (time ./fplll/fplll/latticegen u $i 10 | ./fplll/fplll/fplll -a svp)  &>> timing.txt 
+     echo "We have $i dimensions" >> timing-c2-2xlarge.txt
+     (time ./fplll/fplll/fplll -a svp matrices/matrix$i.txt)  &>> timing-c2-2xlarge.txt 
      
 done
 
