17614da3fb4294cba1eb47e7f4114f009d3ed536
Leo de Castro <ldecastro33@gmail.com>
2017-12-17 15:32:34 -0500

    Runs through many std deviations.

---
+++ check_std_devs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+std_dev=1
+dim=128
+mod=769
+block=2
+
+until [ $std_dev -gt 50 ]
+do
+	echo "Std dev: " $std_dev
+	cvp_enum/bin/enumeration -n$dim -q$mod -s$std_dev -b$block -i"reduced" -p
+
+	if [ -e ./solution_vector.dat ]
+       	then
+		break
+	fi
+done
