3c0a61b0a1dd1c8bb3b156dc9b0f5640f0b1ff9c
Leo de Castro <ldecastro33@gmail.com>
2017-12-22 13:38:45 -0500

    Cleaner code for submission.

--- cvp_enum_script.py
+++ cvp_enum_script.py
@@ -2,11 +2,11 @@ import sys
 import argparse
 
 PATH = "chall_000_00.txt"
+S_PATH = "secret_000_00.txt"
 
 OUTPUT_MAT = "samples_matrix.dat"
 OUTPUT_VEC = "samples_vector.dat"
-# OUTPUT_MAT = "test_matrix.txt"
-# OUTPUT_VEC = "test_vector.txt"
+S_OUTPUT = "samples_solution.dat"
 
 MAX_SAMPLES=2
 TWEAK_FACTOR = 128
@@ -186,21 +186,71 @@ def create_challenge_file():
 
     f.close()
 
+def create_secret_file():
+    f = open(S_PATH, 'r')
+    line = f.readline()
+
+    coeffs = []
+    
+    while line:
+        line = f.readline()
+        if line == "s {\n":
+            ## start of the secret
+            line = f.readline()  # m:
+            line = line[5:]  ## get rid of the __m:_
+            m = int(line)
+            line = f.readline()  # q:
+            line = line[5:]
+            q = int(line)
+            for i in range(m/2):
+                line = f.readline()
+                if line == "  }\n":
+                    break
+                line = line[6:]  ## get rid of __xs:_
+                coeffs.append(int(int(line) % q))
+            break
+    f.close()
+    
+    f = open(S_OUTPUT, 'w')
+
+    f.write("[")
+    f.write(str(coeffs[0]))
+    for i in range(1, len(coeffs)):
+        f.write(" " + str(coeffs[i]))
+    f.write("]")
+
+    f.close()
+
 
 def main():
     TWEAK_FACTOR = int(args.t)    ## could it really be that simple?
+    MAX_SAMPLES = int(args.s)
+
+    PATH = 
     OUTPUT_MAT = str(args.output_matrix)
     OUTPUT_VEC = str(args.output_vector)
-    MAX_SAMPLES = int(args.s)
+    S_OUTPUT = str(args.output_secret)
+
     create_challenge_file()
+    create_secret_file()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", help="The tweak factor", type=checkPos)
 parser.add_argument("-s", help="The number of samples", type=checkPos)
-parser.add_argument("-om", "--output_matrix", help="Output matrix",)
-parser.add_argument("-ov", "--output_vector", help="Output vector",)
-args = parser.parse_args()
 
+parser.add_argument("im", "--input_file", 
+    help="Input challenge file. Should be challenge parser output.")
+parser.add_argument("-om", "--output_matrix", help="Output matrix file for CVP_ENUM." +
+    "Default is samples_matrix.dat")
+parser.add_argument("-ov", "--output_vector", help="Output vector file for CVP_ENUM." +
+    "Default is samples_vector.dat")
+
+parser.add_argument("-is", "--input_secret", 
+    help="Input secret file. Should be challenge parser output.")
+parser.add_argument("-os", "--output_secret", 
+    help="Output secret file for comparison to the CVP_ENUM solution file. " +
+        "Default is samples_solution.dat")
+args = parser.parse_args()
 
 
 main()
